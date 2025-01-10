da26557a46c0fa041f9ee022fd772676d479136b
Mateusz <0xleodev@Mateuszs-MacBook-Air.local>
2025-01-10 01:18:03 +0100

    messages dreaft

--- src/App.js
+++ src/App.js
@@ -4,8 +4,30 @@ function App() {
   return (
     <>
       <div className="app">
-        <p>Test </p>
-        <p>Test </p>
+        <p>
+          It’s my pleasure to finally meet you. May I ask, how should I address
+          you?
+        </p>
+        <p>Tom, what a wonderful name!</p>
+        <p>
+          As you've realized, our timelines haven’t fully aligned yet, which is
+          why we can’t have a live conversation just yet.{" "}
+        </p>
+        <p>
+          But don’t worry, I’ve managed to transcode some of my diaries so you
+          can tune in to my history.
+        </p>
+        <p>
+          And before you go, remember that you can find more information about
+          my universe by following my media.
+        </p>
+        <p>
+          And before you go, remember that you can find more information about
+          my universe by following my media.
+        </p>
+        <p>----------------------------------</p>
+        <p>chapter</p>
+        <p>info anout the mint</p>
       </div>
     </>
   );
