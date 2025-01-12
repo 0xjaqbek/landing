c50ffe45653e8b4e3399a9941e9802329e9a85d4
Mateusz <0xleodev@Mateuszs-MacBook-Air.local>
2025-01-12 15:54:54 +0100

    language change

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
     <div>
-      <h1>404 - Strona nie znaleziona</h1>
-      <p>Przepraszamy, ta strona nie istnieje.</p>
+      <h1>Page Not Found</h1>
+      <p>The page you're looking for doesn't exist.</p>
+      <Link to="/">
+        <button>Go back to Home</button>
+      </Link>
     </div>
   );
 };
