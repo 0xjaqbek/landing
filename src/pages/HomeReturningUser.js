54e2cf436a1ac5bd61164af7fe575ebb84af3d32
Mateusz <0xleodev@Mateuszs-MacBook-Air.local>
2025-01-13 00:57:42 +0100

    sent message function

--- src/pages/HomeReturningUser.js
+++ src/pages/HomeReturningUser.js
@@ -4,49 +4,84 @@ import { useLocation, Link } from "react-router-dom";
 const HomeReturningUser = (props) => {
   const [name, setName] = useState(props.userName);
 
+  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState("");
+
+  const handleSendMessage = (messageTxt, messageSender) => {
+    const newMessageObject = {
+      id: Date.now(),
+      senderName: messageSender,
+      text: messageTxt,
+      time: new Date(Date.now()).toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+      }),
+    };
+    setMessages((prev) => [...prev, newMessageObject]);
+    setInput("");
+  };
+
+  useEffect(() => {
+    handleSendMessage(`Welcome back, ${name}!`, "Aria");
+    handleSendMessage(
+      `As you've realized, our timelines haven’t fully aligned yet, which is
+        why we can’t have a live conversation just yet.`,
+      "Aria"
+    );
+    handleSendMessage(
+      `But don’t worry, I’ve managed to transcode some of my diaries so you can
+        tune in to my history.`,
+      "Aria"
+    );
+    handleSendMessage(
+      `And before you go, remember that you can find more information about my
+        universe by following my media.`,
+      "Aria"
+    );
+    handleSendMessage(
+      <div>
+        <h3>Choose a chapter to start reading:</h3>
+        <Link to="/chapter1">
+          <button>Chapter 1</button>
+        </Link>
+        <Link to="/chapter2">
+          <button>Chapter 2</button>
+        </Link>
+        <Link to="/chapter3">
+          <button>Chapter 3</button>
+        </Link>
+        <Link to="/chapter4">
+          <button>Chapter 4</button>
+        </Link>
+        <Link to="/chapter5">
+          <button>Chapter 5</button>
+        </Link>
+        <Link to="/chapter6">
+          <button>Chapter 6</button>
+        </Link>
+        <Link to="/chapter7">
+          <button>Chapter 7</button>
+        </Link>
+        <Link to="/chapter8">
+          <button>Chapter 8</button>
+        </Link>
+      </div>,
+      "Aria"
+    );
+  }, []);
+
   return (
     <div>
-      <h2>Welcome back, {name}!</h2>
-
-      <h3>
-        As you've realized, our timelines haven’t fully aligned yet, which is
-        why we can’t have a live conversation just yet.{" "}
-      </h3>
-      <h3>
-        But don’t worry, I’ve managed to transcode some of my diaries so you can
-        tune in to my history.
-      </h3>
-      <h3>
-        And before you go, remember that you can find more information about my
-        universe by following my media.
-      </h3>
-
-      <h3>Choose a chapter to start reading:</h3>
-
-      <Link to="/chapter1">
-        <button>Chapter 1</button>
-      </Link>
-      <Link to="/chapter2">
-        <button>Chapter 2</button>
-      </Link>
-      <Link to="/chapter3">
-        <button>Chapter 3</button>
-      </Link>
-      <Link to="/chapter4">
-        <button>Chapter 4</button>
-      </Link>
-      <Link to="/chapter5">
-        <button>Chapter 5</button>
-      </Link>
-      <Link to="/chapter6">
-        <button>Chapter 6</button>
-      </Link>
-      <Link to="/chapter7">
-        <button>Chapter 7</button>
-      </Link>
-      <Link to="/chapter8">
-        <button>Chapter 8</button>
-      </Link>
+      <div>
+        {messages.map((message) => (
+          <div key={message.id}>
+            <div>{message.time}</div>
+            <div>{message.senderName}</div>
+            <div>{message.text}</div>
+            <div>----------</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
