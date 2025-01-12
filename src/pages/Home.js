ec6d7a56b91debfbf8ad5ca9d6e7607070bac2d4
Mateusz <0xleodev@Mateuszs-MacBook-Air.local>
2025-01-12 19:41:03 +0100

    better render logic

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,13 +4,19 @@ import HomeReturningUser from "./HomeReturningUser";
 
 const Home = () => {
   const [userName, setUserName] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const storedUserName = localStorage.getItem("userName");
     if (storedUserName) {
       setUserName(storedUserName);
     }
+    setIsLoading(false);
   }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   if (userName) {
     return <HomeReturningUser userName={userName} />;
