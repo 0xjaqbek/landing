62b6bb172419624217f7750151e04bcea4f12d58
Mateusz <0xleodev@Mateuszs-MacBook-Air.local>
2025-02-01 13:20:31 +0100

    elements draft plot

--- src/pages/Chapter1.js
+++ src/pages/Chapter1.js
@@ -7,65 +7,7 @@ const Chapter1 = () => {
 
   return (
     <div>
-      <h1>Rozdział 1: Początek Przygody</h1>
-      <p>
-        To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz wprowadzić
-        treść rozdziału, np. tekst opisujący wstęp do Twojej historii lub
-        aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.
-      </p>
-
-      <h2>Co się wydarzy dalej?</h2>
-      <p>
-        Przed Tobą jeszcze wiele nieznanych przygód. Kliknij poniżej, aby
-        przejść do kolejnego rozdziału:
-      </p>
-
-      <nav>
-        <ul>
-          <li>
-            <Link to="/chapter2">Przejdź do Rozdziału 2</Link>
-          </li>
-          <li>
-            <Link to="/">Powrót do Strony Głównej</Link>
-          </li>
-        </ul>
-      </nav>
+      <p>test</p>
     </div>
   );
 };
--- src/pages/Story.js
+++ src/pages/Story.js
@@ -1,73 +1,56 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, Link } from "react-router-dom";
 import usePageVisitTracking from "../CustomHooks/usePageVisitTracking"; // Adjust the import path
+import "./story.css";
 
-const Chapter1 = () => {
-  usePageVisitTracking(useLocation().pathname);
-
+const Story = () => {
   return (
     <div>
-      <h1>Rozdział 1: Początek Przygody</h1>
-      <p>
-        To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz wprowadzić
-        treść rozdziału, np. tekst opisujący wstęp do Twojej historii lub
-        aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.To jest pierwszy rozdział w Twojej aplikacji! Tutaj możesz
-        wprowadzić treść rozdziału, np. tekst opisujący wstęp do Twojej historii
-        lub aplikacji.
-      </p>
-
-      <h2>Co się wydarzy dalej?</h2>
-      <p>
-        Przed Tobą jeszcze wiele nieznanych przygód. Kliknij poniżej, aby
-        przejść do kolejnego rozdziału:
-      </p>
-
-      <nav>
-        <ul>
-          <li>
-            <Link to="/chapter2">Przejdź do Rozdziału 2</Link>
-          </li>
-          <li>
-            <Link to="/">Powrót do Strony Głównej</Link>
-          </li>
-        </ul>
-      </nav>
+      <div className="header">
+        <button className="button-header">CHAPTER 1</button>
+        <button className="button-header">CHAPTER 2</button>
+        <button className="button-header">CHAPTER 3</button>
+        <button className="button-header">CHAPTER 4</button>
+        <button className="button-header">CHAPTER 5</button>
+        <button className="button-header">CHAPTER 6</button>
+        <button className="button-header">CHAPTER 7</button>
+        <button className="button-header">CHAPTER 8</button>
+      </div>
+      <div className="baner">
+        <p className="banner-header">What is Lorem Ipsum?</p>
+        <p className="banner-text">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an
+        </p>
+        <img className="banner-img" />
+        <button className="banner-button">MINT</button>
+      </div>
+      <div className="plot">
+        Why do we use it? It is a long established fact that a reader will be
+        distracted by the readable content of a page when looking at its layout.
+        The point of using Lorem Ipsum is that it has a more-or-less normal
+        distribution of letters, as opposed to using 'Content here, content
+        here', making it look like readable English. Many desktop publishing
+        packages and web page editors now use Lorem Ipsum as their default model
+        text, and a search for 'lorem ipsum' will uncover many web sites still
+        in their infancy. Various versions have evolved over the years,
+        sometimes by accident, sometimes on purpose (injected humour and the
+        like). Where does it come from? Contrary to popular belief, Lorem Ipsum
+        is not simply random text. It has roots in a piece of classical Latin
+        literature from 45 BC, making it over 2000 years old. Richard
+        McClintock, a Latin professor at Hampden-Sydney College in Virginia,
+        looked up one of the more obscure Latin words, consectetur, from a Lorem
+        Ipsum passage, and going through the cites of the word in classical
+        literature, discovered the undoubtable source. Lorem Ipsum comes from
+        sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The
+        Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a
+        treatise on the theory of ethics, very popular during the Renaissance.
+        The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes
+        from a line in section 1.10.32
+      </div>
     </div>
   );
 };
 
-export default Chapter1;
+export default Story;
