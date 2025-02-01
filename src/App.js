702a90b830ad5a991f41f600a87ce4b57f71f551
Mateusz <0xleodev@Mateuszs-MacBook-Air.local>
2025-02-01 17:20:08 +0100

    tree clean up

--- src/App.js
+++ src/App.js
@@ -3,14 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Chat from "./Pages/Chat";
 import Story from "./Pages/Story";
-import Chapter1 from "./Pages/Chapter1";
-import Chapter2 from "./Pages/Chapter2";
-import Chapter3 from "./Pages/Chapter3";
-import Chapter4 from "./Pages/Chapter4";
-import Chapter5 from "./Pages/Chapter5";
-import Chapter6 from "./Pages/Chapter6";
-import Chapter7 from "./Pages/Chapter7";
-import Chapter8 from "./Pages/Chapter8";
 import NotFound from "./Pages/NotFound";
 
 import backgroundImage1 from "./Asets/background1.png";
@@ -27,14 +19,6 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/chat" element={<Chat />} />
             <Route path="/story" element={<Story />} />
-            <Route path="/chapter1" element={<Chapter1 />} />
-            <Route path="/chapter2" element={<Chapter2 />} />
-            <Route path="/chapter3" element={<Chapter3 />} />
-            <Route path="/chapter4" element={<Chapter4 />} />
-            <Route path="/chapter5" element={<Chapter5 />} />
-            <Route path="/chapter6" element={<Chapter6 />} />
-            <Route path="/chapter7" element={<Chapter7 />} />
-            <Route path="/chapter8" element={<Chapter8 />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
