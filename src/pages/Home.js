4ab247b3fc2d212e029a5e8abe3ec3e77d8f6267
Mateusz <0xleodev@Mateuszs-MacBook-Air.local>
2025-02-03 15:57:34 +0100

    check for username

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,50 +7,63 @@ import ariaProfile from "../Asets/ariaProfile.png";
 import frame from "../Asets/frame.svg";
 
 function Home() {
-  const [userName, setUserName] = useState("");
+  const [userName, setUserName] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const storedUserName = localStorage.getItem("userName");
     if (storedUserName) {
+      console.log("user found: " + storedUserName);
       setUserName(storedUserName);
+    } else {
+      console.log("new user");
     }
+
     setIsLoading(false);
   }, []);
 
   const navigate = useNavigate();
 
   if (isLoading) {
-    return <div className="">Loading...</div>;
+    return <div>Loading...</div>;
   } else {
-  }
-  return (
-    <>
-      <div className="landing-page">
-        <div className="topGroup">
-          <img className="ariaProfile" src={ariaProfile} alt="ariaProfile" />
-          <div className="frameContainer">
-            <div className="headerTxt">
-              <p className="line">
-                Hello adventurer! <br /> MY NAME IS ARIA INGRAM.
-              </p>
-              <p className="line">
-                <span className="highlight"> ready</span> to follow my story?
-              </p>
-            </div>
+    return (
+      <>
+        <div className="landing-page">
+          <div className="topGroup">
+            <img className="ariaProfile" src={ariaProfile} alt="ariaProfile" />
+            <div className="frameContainer">
+              <div className="headerTxt">
+                {userName ? (
+                  <p className="line">
+                    <p className="line">
+                      Hello, {userName}! <br />
+                      Good to see you again.
+                    </p>
+                  </p>
+                ) : (
+                  <p className="line">
+                    Hello adventurer! <br /> MY NAME IS ARIA INGRAM.
+                  </p>
+                )}
+                <p className="line">
+                  <span className="highlight"> ready</span> to follow my story?
+                </p>
+              </div>
 
-            <img className="frame" src={frame} alt="frame" />
+              <img className="frame" src={frame} alt="frame" />
+            </div>
           </div>
+          <button onClick={() => navigate("/story")} className="button">
+            ENTER STORY
+          </button>
+          <button onClick={() => navigate("/chat")} className="button button2">
+            LETS TALK
+          </button>
         </div>
-        <button onClick={() => navigate("/story")} className="button">
-          ENTER STORY
-        </button>
-        <button onClick={() => navigate("/chat")} className="button button2">
-          LETS TALK
-        </button>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
 
 export default Home;
