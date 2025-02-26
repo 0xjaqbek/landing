b5f2e08ac903590a504a8acafe30824b2afa70c1
0xjaqbek <jaqbek.eth@gmail.com>
2025-02-26 20:07:31 +0100

    hiding plans

--- src/pages/I.js
+++ src/pages/I.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import navBack from "../Asets/navBack.svg";
-import "./story.css"; 
+import "./story.css";
 
-const Airdrop = () => {
+const I = () => {
   const navigate = useNavigate();
 
   return (
@@ -16,7 +16,7 @@ const Airdrop = () => {
       />
       <div className="plot-big-cointerner">
         <div className="plot-cointerner">
-          <h1 className="title">What is Lorem Ipsum? </h1>
+          <h1 className="title">What is Lorem Ipsum?</h1>
           <p className="plot">
           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
           </p>
@@ -26,4 +26,4 @@ const Airdrop = () => {
   );
 };
 
-export default Airdrop;
+export default I;
--- src/pages/II.js
+++ src/pages/II.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import navBack from "../Asets/navBack.svg";
-import "./story.css";
+import "./story.css"; 
 
-const Claim = () => {
+const II = () => {
   const navigate = useNavigate();
 
   return (
@@ -16,7 +16,7 @@ const Claim = () => {
       />
       <div className="plot-big-cointerner">
         <div className="plot-cointerner">
-          <h1 className="title">What is Lorem Ipsum?</h1>
+          <h1 className="title">What is Lorem Ipsum? </h1>
           <p className="plot">
           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
           </p>
@@ -26,4 +26,4 @@ const Claim = () => {
   );
 };
 
-export default Claim;
+export default II;
