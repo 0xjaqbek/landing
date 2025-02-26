8ffac8b2c631ae2e646367faa8ee44b91d93305c
0xjaqbek <jaqbek.eth@gmail.com>
2025-02-26 19:49:02 +0100

    C&A

--- src/pages/Airdrop.js
+++ src/pages/Airdrop.js
@@ -16,7 +16,7 @@ const Airdrop = () => {
       />
       <div className="plot-big-cointerner">
         <div className="plot-cointerner">
-          <h1 className="title">Aria I love U </h1>
+
           <p className="plot">
           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
           </p>
--- src/pages/Claim.js
+++ src/pages/Claim.js
@@ -16,7 +16,7 @@ const Claim = () => {
       />
       <div className="plot-big-cointerner">
         <div className="plot-cointerner">
-          <h1 className="title">I love Aria</h1>
+
           <p className="plot">
           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
           </p>
